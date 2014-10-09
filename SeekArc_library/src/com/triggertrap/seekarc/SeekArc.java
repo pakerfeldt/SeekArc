0bbbf40e39d9b5eb6167057d4e950b86cde5174b
Patrik Åkerfeldt <patrik.akerfeldt@gmail.com>
2014-10-09 21:44:33 +0200

    Getter for progress
    Adds a getter for the progress value.

--- SeekArc_library/src/com/triggertrap/seekarc/SeekArc.java
+++ SeekArc_library/src/com/triggertrap/seekarc/SeekArc.java
@@ -455,6 +455,10 @@ public class SeekArc extends View {
 		updateProgress(progress, false);
 	}
 
+	public int getProgress() {
+		return mProgress;
+	}
+
 	public int getProgressWidth() {
 		return mProgressWidth;
 	}
